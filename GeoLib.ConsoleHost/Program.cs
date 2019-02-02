1779a197eb5c562720e0bf39985a302e45328a30
Adrian Puscasu <apu@f5it.ro>
2019-02-03 00:13:44 +0200

    - adds hosting projects; - functional ConsoleHost project;

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GeoLib.Services;
+using System.ServiceModel;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+
+            Console.WriteLine("Services started. Pres [Enter] to exit.");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
