cbe0170f7820874c7252d729adfe4835a44adbbc
Adrian Puscasu <apu@f5it.ro>
2019-02-03 12:35:48 +0200

    - adds net.tcp binding in WebHost project;

---
+++ GeoLib.WebHost/CustomHostFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace GeoLib.WebHost
+{
+    public class CustomHostFactory: ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            var host = new ServiceHost(serviceType, baseAddresses);
+            return host;
+        }
+    }
+}
---
+++ GeoLib.WebHost/addNet.TcpBinding.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module WebAdministration
+
+$websites = Get-ChildItem 'IIS:\Sites'
+$site = $websites | Where-object { $_.Name -eq 'Default Web Site' }
+$netTcpExists = [bool]($site.bindings.Collection | ? { $_.bindingInformation -eq '808:*' -and $_.protocol -eq 'net.tcp' })
+
+if (!$netTcpExists)
+{
+    Write-Output "Net TCP binding does not exist. Creating binding now..."
+    # Create the binding
+    New-ItemProperty 'IIS:\Sites\Default Web Site' -name bindings -Value @{protocol="net.tcp";bindingInformation="808:*"}
+
+    Write-Output "Binding created"
+}
+else
+{
+    Write-Output "TCP Binding already exists"
+}
+
+Write-Output "Updating enabled protocols..."
+
+Set-ItemProperty 'IIS:\sites\Default Web Site' -name EnabledProtocols -Value "http,net.tcp"
+
+Write-Output "Enabled protocols updated"
